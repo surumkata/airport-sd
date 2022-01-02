cb960879a13f969b1814d07a48d860826df63b3c
realRunlo <gteixeira.pereira@gmail.com>
2022-01-02 18:23:52 +0000

    inicio parser cliente input

--- src/Cliente.java
+++ src/Cliente.java
@@ -8,19 +8,30 @@ import java.util.Arrays;
 
 public class Cliente {
 
-/*
-    public static Voo parseLine (String userInput) {
+    private void parser(String userInput){
         String[] tokens = userInput.split(" ");
 
-        if (tokens[3].equals("null")) tokens[3] = null;
+        if(tokens.length==1 && tokens[0].equals("voos")){
+            //todo: lista de voos origem->destino
+        }else if(tokens.length==2){
+            if(tokens[0].equals("encerra")){
+                //todo: encerra dia token[1](admin)
+            }else if(tokens[0].equals("cancela")){
+                //todo: cancela reserva codReserva = token[1] (utilizador)
+            }
+        }else if(tokens.length==3){
+            if(tokens[0].equals("login")){
+                //todo: login name = token[1] pass = token[2]
+            }else if(tokens[0].equals("registo")){
+                //todo: registo name = token[1] pass = token[2]
+            }else if(tokens[0].equals("reserva")){
+                //todo: reserva cidades(separadas ;) = token[1] datas(separadas ;) = token[2]
+            }
+        }else if(tokens.length==4 && tokens[0].equals("addvoo")){
+            //todo: adiciona voo origem = token[1] destino = token[2] capacidade = token[3]
+        }
 
-        return new Voo(
-                tokens[0],
-                Integer.parseInt(tokens[1]),
-                Long.parseLong(tokens[2]),
-                tokens[3],
-                new ArrayList<>(Arrays.asList(tokens).subList(4, tokens.length)));
-    }*/
+    }
 
     public static void main (String[] args) throws IOException {
         Socket socket = new Socket("localhost", 12345);
